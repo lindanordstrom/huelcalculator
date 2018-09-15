2320f22a00b232c93d60eb6bf29856f45f799dfe
Linda Nordström <lindanordstrom86@gmail.com>
2018-09-16 01:55:33 +0200

    tests

--- HuelCalculatorTests/DataObjects/UserTests.swift
+++ HuelCalculatorTests/DataObjects/UserTests.swift
@@ -14,22 +14,50 @@ class UserTests: XCTestCase {
     private var testObject = User.self
     
     func test_getActivityString() {
-        var result = testObject.ActivityLevel.getActivityString(activity: .sedentary)
+        var result = testObject.ActivityLevel.sedentary.description
         XCTAssertEqual(result, Constants.User.sedentaryActive)
 
-        result = testObject.ActivityLevel.getActivityString(activity: .lightly)
+        result = testObject.ActivityLevel.lightly.description
         XCTAssertEqual(result, Constants.User.lightlyActive)
 
-        result = testObject.ActivityLevel.getActivityString(activity: .moderately)
+        result = testObject.ActivityLevel.moderately.description
         XCTAssertEqual(result, Constants.User.moderatelyActive)
 
-        result = testObject.ActivityLevel.getActivityString(activity: .very)
+        result = testObject.ActivityLevel.very.description
         XCTAssertEqual(result, Constants.User.veryActive)
 
-        result = testObject.ActivityLevel.getActivityString(activity: .extra)
+        result = testObject.ActivityLevel.extra.description
         XCTAssertEqual(result, Constants.User.extraActive)
-
-        result = testObject.ActivityLevel.getActivityString(activity: .count)
-        XCTAssertEqual(result, Constants.User.selectActivity)
+        
+        result = testObject.ActivityLevel.count.description
+        XCTAssertEqual(result, "")
+    }
+    
+    func test_count() {
+        let result = testObject.ActivityLevel.itemCount
+        XCTAssertEqual(result, 5)
+    }
+    
+    func test_itemAtIndex() {
+        var result = testObject.ActivityLevel.itemAt(rawValue: 0) as? User.ActivityLevel
+        XCTAssertEqual(result, testObject.ActivityLevel.sedentary)
+        
+        result = testObject.ActivityLevel.itemAt(rawValue: 1)  as? User.ActivityLevel
+        XCTAssertEqual(result, testObject.ActivityLevel.lightly)
+        
+        result = testObject.ActivityLevel.itemAt(rawValue: 2)  as? User.ActivityLevel
+        XCTAssertEqual(result, testObject.ActivityLevel.moderately)
+        
+        result = testObject.ActivityLevel.itemAt(rawValue: 3)  as? User.ActivityLevel
+        XCTAssertEqual(result, testObject.ActivityLevel.very)
+        
+        result = testObject.ActivityLevel.itemAt(rawValue: 4)  as? User.ActivityLevel
+        XCTAssertEqual(result, testObject.ActivityLevel.extra)
+        
+        result = testObject.ActivityLevel.itemAt(rawValue: 5)  as? User.ActivityLevel
+        XCTAssertEqual(result, testObject.ActivityLevel.count)
+        
+        result = testObject.ActivityLevel.itemAt(rawValue: 6)  as? User.ActivityLevel
+        XCTAssertNil(result)
     }
 }
